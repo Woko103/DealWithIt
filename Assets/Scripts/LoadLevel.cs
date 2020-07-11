895cc91fd317adddad02d757716d997f834fc48d
Felor <antti.hamara@gmail.com>
2020-07-11 18:26:33 +0300

    Title screen is now functional

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        int sceneNum = scene.buildIndex;
+
+        if (sceneNum >= SceneManager.sceneCountInBuildSettings - 1)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneNum + 1);
+        }
+    }
+
+    public void ResetLevel()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        int sceneNum = scene.buildIndex;
+        SceneManager.LoadScene(sceneNum);
+    }
+}
