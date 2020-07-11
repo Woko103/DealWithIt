ce097ff47bc50e1ddf2b83fc48023266145a6cdb
El Controlador <elroquemadridista@gmail.com>
2020-07-12 00:35:54 +0200

    spawners updated

--- Assets/Scripts/dealGenerator.cs
+++ Assets/Scripts/dealGenerator.cs
@@ -10,9 +10,53 @@ public class dealGenerator : MonoBehaviour
 
     void Start()
     {
-        dealPositions.Add(new Vector3(0,0,10));
+        //dealPositions.Add(new Vector3(0,0,10));
         //dealPositions.Add(new Vector3(5,0,13));
         //dealPositions.Add(new Vector3(-5,0,13));
+
+        //FIRST
+        dealPositions.Add(new Vector3(-12.5f, 0, 11.06f));
+        dealPositions.Add(new Vector3(-12.5f, 0, 27.77f));
+        dealPositions.Add(new Vector3(-12.5f, 0, -14.09f));
+        dealPositions.Add(new Vector3(-12.5f, 0, -30.8f));
+        dealPositions.Add(new Vector3(12.5f, 0, -22));
+        dealPositions.Add(new Vector3(12.5f, 0, 20));
+        //SECOND
+        dealPositions.Add(new Vector3(-29.66f, 0, 9));
+        dealPositions.Add(new Vector3(-29.66f, 0, 15));
+        dealPositions.Add(new Vector3(-29.66f, 0, 23));
+        dealPositions.Add(new Vector3(-29.66f, 0, 31));
+        dealPositions.Add(new Vector3(-29.66f, 0, 31));
+        dealPositions.Add(new Vector3(-29.66f, 0, -11));
+        dealPositions.Add(new Vector3(-29.66f, 0, -17));
+        dealPositions.Add(new Vector3(-29.66f, 0, -28));
+        dealPositions.Add(new Vector3(-29.66f, 0, -34));
+        dealPositions.Add(new Vector3(27.1f, 0, -35));
+        dealPositions.Add(new Vector3(27.1f, 0, -28));
+        dealPositions.Add(new Vector3(27.1f, 0, -18));
+        dealPositions.Add(new Vector3(27.1f, 0, -10.5f));
+        dealPositions.Add(new Vector3(27.1f, 0, -35));
+        dealPositions.Add(new Vector3(27.1f, 0, 7.5f));
+        dealPositions.Add(new Vector3(27.1f, 0, 14));
+        dealPositions.Add(new Vector3(27.1f, 0, 23.5f));
+        dealPositions.Add(new Vector3(27.1f, 0, 29.5f));
+        dealPositions.Add(new Vector3(36.6f, 0, 27.5f));
+        dealPositions.Add(new Vector3(36.6f, 0, 27.5f));
+        dealPositions.Add(new Vector3(36.6f, 0, 10.6f));
+        dealPositions.Add(new Vector3(36.6f, 0, -14.4f));
+        dealPositions.Add(new Vector3(36.6f, 0, -31.7f));
+        dealPositions.Add(new Vector3(-41, 0, -22));
+        dealPositions.Add(new Vector3(-41, 0, -20));
+        //THIRD
+        dealPositions.Add(new Vector3(-26.09f, 0, 40));
+        dealPositions.Add(new Vector3(0, 0, 40));
+        dealPositions.Add(new Vector3(26.09f, 0, 40));
+        dealPositions.Add(new Vector3(-26.09f, 0, -42));
+        dealPositions.Add(new Vector3(26.09f, 0, -42));
+        dealPositions.Add(new Vector3(-2.5f, 0, 19.6f));
+        dealPositions.Add(new Vector3(2.5f, 0, 19.6f));
+        dealPositions.Add(new Vector3(-2.5f, 0, -22.1f));
+        dealPositions.Add(new Vector3(2.5f, 0, -22.1f)); 
     }
 
     void FixedUpdate ()
