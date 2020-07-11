8afe0a3a5cb4e4b88410b883fe298281f7a23551
Woko103 <43748015+Woko103@users.noreply.github.com>
2020-07-11 15:33:12 +0200

    movement and turning added

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class gameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+
+    public void endGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Invoke("showLoseScreen", 1f);
+        }
+    }
+
+    void showLoseScreen ()
+    {
+        Debug.Log("Show lose Screen");
+    }
+}
---
+++ Assets/Scripts/pauseMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public AudioSource mainAudio;
+    void Update()
+    {
+        //Resume
+        if (Input.anyKeyDown && Time.timeScale == 0)
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            mainAudio.Play();
+        }
+        //Pause
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+            {
+                pauseMenuUI.SetActive(true);
+                Time.timeScale = 0f;
+                mainAudio.Pause();
+            }
+        }
+    }
+}
