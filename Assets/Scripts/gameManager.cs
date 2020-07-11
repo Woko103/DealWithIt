b5413aae2b258a6c0616baa1a25ca07432f56646
Woko103 <43748015+Woko103@users.noreply.github.com>
2020-07-11 23:32:32 +0200

    Lose screen working and minor changes

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -5,18 +5,34 @@ using UnityEngine.UI;
 public class gameManager : MonoBehaviour
 {
     bool gameHasEnded = false;
+    public GameObject loseScreen;
+    public GameObject winScreen;
 
     public void endGame()
     {
         if (gameHasEnded == false)
         {
             gameHasEnded = true;
-            Invoke("showLoseScreen", 1f);
+            Invoke("showLoseScreen", 0.5f);
         }
     }
 
     void showLoseScreen ()
     {
-        Debug.Log("Show lose Screen");
+        loseScreen.SetActive(true);
+    }
+
+    public void winGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Invoke("showWinScreen", 0.5f);
+        }
+    }
+
+    void showWinScreen ()
+    {
+        winScreen.SetActive(true);
     }
 }
--- Assets/Scripts/playerCollision.cs
+++ Assets/Scripts/playerCollision.cs
@@ -20,11 +20,11 @@ public class playerCollision : MonoBehaviour
                 if(transform.rotation.y > -0.2 && transform.rotation.y < 0.2)
                     collisionInfo.transform.position = transform.position + new Vector3(0,0.75f,0.6f);
                 else if(transform.rotation.y > 0.6 && transform.rotation.y < 0.8)
-                    collisionInfo.transform.position = transform.position + new Vector3(0,0.4f,0);
+                    collisionInfo.transform.position = transform.position + new Vector3(0.6f,0.75f,0);
                 else if(transform.rotation.y > -0.8 && transform.rotation.y < -0.6)
-                    collisionInfo.transform.position = transform.position + new Vector3(0,0.4f,0);
+                    collisionInfo.transform.position = transform.position + new Vector3(0.6f,0.75f,0);
                 else
-                    collisionInfo.transform.position = transform.position + new Vector3(0,0.4f,0);
+                    collisionInfo.transform.position = transform.position + new Vector3(0,0.75f,-0.6f);
 
                 collisionInfo.transform.parent = GameObject.Find("Player").transform;
                 collisionInfo.rigidbody.isKinematic = true;
