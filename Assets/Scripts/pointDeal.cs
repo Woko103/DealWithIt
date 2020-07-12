dc3b171018d4efa7758658ff28dfc15725fdcc22
Woko103 <43748015+Woko103@users.noreply.github.com>
2020-07-12 12:34:19 +0200

    Arrow points the deal

---
+++ Assets/Scripts/pointDeal.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pointDeal : MonoBehaviour
+{
+    public GameObject deal;
+
+    void FixedUpdate()
+    {
+        transform.rotation = Quaternion.LookRotation (deal.transform.position - transform.position);
+    }
+}
